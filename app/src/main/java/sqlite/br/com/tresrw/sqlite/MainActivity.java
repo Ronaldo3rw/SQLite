003806599b97aa1930c81eb725c965101d2c7f8a
Ronaldo Cagliari <rdias3rw@gmail.com>
2018-01-10 22:10:38 -0200

    Commit inicial

---
+++ app/src/main/java/sqlite/br/com/tresrw/sqlite/MainActivity.java
@@ -0,0 +1,14 @@
+package sqlite.br.com.tresrw.sqlite;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
