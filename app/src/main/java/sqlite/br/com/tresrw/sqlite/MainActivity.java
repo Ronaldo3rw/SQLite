dc976709416b58f3c891d166c4cee8889569852f
Ronaldo Cagliari <rdias3rw@gmail.com>
2018-01-10 23:46:58 -0200

    Commit teste 2222

--- app/src/main/java/sqlite/br/com/tresrw/sqlite/MainActivity.java
+++ app/src/main/java/sqlite/br/com/tresrw/sqlite/MainActivity.java
@@ -1,6 +1,7 @@
 package sqlite.br.com.tresrw.sqlite;
 
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +11,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Criar Banco de Dados SQLite
+        // Variável bancoDados do tipo SQLiteDataBase =
+        SQLiteDatabase bancoDados = openOrCreateDatabase("app", MODE_PRIVATE, null);
+
+
+        // Testes de sincronização do Android Studio com o GitHub
+
+
+
+
     }
 }
